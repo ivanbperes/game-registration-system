1fa72b0fcc4c93abfb255300689e08bfb1e0a0fc
Ecenture <ivanbperes81@gmail.com>
2023-01-17 19:22:36 -0300

    Restore last commit

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Running game registration system");
 console.log("connecting application to database");
 console.log("A commit to test git pull");
 console.log("Testing git push from local to web");
-console.log("Yet another test from local to web");
